cd2018276c24a087c813349b94f01a174594570e
hamcrack <hamishcrackett@gmail.com>
2025-05-05 11:26:38 +0200

    Done and tidy

--- test.py
+++ test.py
@@ -20,22 +20,12 @@ encoded_image = model.encode_image(image)
 
 prompts = ["Take the scissors",  "Pick the pen" , "Move the controller", "Hold the screwdriver", "Roll the tape"]
 
-# val = input("Please enter prompt: ")
-# print("Processing - '" + val + "' prompt...")
-
-# # Generate caption
-# caption = model.caption(encoded_image)["caption"]
-# print("Caption:", caption)
-
 for val in prompts:
-  # Ask questions
   answer1 = model.query(encoded_image, "Please detect the item in the statement: " + val + ", and return the pixel coordinates of its bounding box.")["answer"]
-  print("Answer1:", answer1)
+  # print("Answer1:", answer1)
   answer2 = model.query(encoded_image, "You are an expert object detector. Please detect the item mentioned in the statement and return the pixel coordinates of its bounding box. The statment is: " + val)["answer"]
-  print("Answer2:", answer2)
-  
+  # print("Answer2:", answer2)
   answer = model.query(encoded_image, "Which bounding box best highlights the item mentioned in the statement: " + val + ". " + answer1 + " or " + answer2)["answer"]
-  
   print("Answer:", answer)
 
   cv2_image = cv2.cvtColor(cv2.imread("2_VLM_Scenario.jpeg"), cv2.COLOR_BGR2RGB)
@@ -54,9 +44,6 @@ for val in prompts:
 
   # Draw bounding box on the image
   cv2.rectangle(cv2_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-  # cv2.imshow("Image", cv2_image)
-  # cv2.waitKey(0)
 
   filename = val + '.jpg'
   print("Saving image as:", filename)
